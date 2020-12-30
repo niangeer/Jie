373b25004453649ce83ccb9c40e52985871b2108
niangeer <898609031@qq.com>
2020-12-30 23:35:14 +0800

    带文件版本通讯录

---
+++ 2020_12_30/main.c
@@ -0,0 +1,51 @@
+#include "contact.h"
+
+int main()
+{
+	contact_t* ct = InitContact();
+	int quit = 0;
+	int select = 0;
+	while (!quit)
+	{
+		Menu();
+		scanf("%d", &select);
+		switch (select)
+		{
+		case 0:
+			quit = 1;
+			SaveContact(ct);
+			DestroyContact(ct);
+			printf("ByeBye!\n");
+			break;
+		case 1:
+			AddPerson(&ct);
+			break;
+		case 2:
+			DelPerson(ct);
+			break;
+		case 3:
+			Search(ct);
+			break;
+		case 4:
+			ModPerson(ct);
+			break;
+		case 5:
+			ShowContact(ct);
+			break;
+		case 6:
+			ClearContact(ct);
+			break;
+		case 7:
+			SortContact(ct);
+			break;
+		case 8:
+			SaveContact(ct);
+			break;
+		default:
+			printf("输入错误，请重新选择!\n");
+			break;
+		}
+	}
+	system("pause");
+	return 0;
+}
