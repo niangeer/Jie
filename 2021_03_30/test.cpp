92533b13086390e617fa042509cae2bbd022e69e
niangeer <898609031@qq.com>
2021-03-30 22:54:28 +0800

    OJ题：是否回文字符串

---
+++ 2021_03_30/test.cpp
@@ -0,0 +1,56 @@
+//题目：验证回文串
+//
+//给定一个字符串，验证它是否是回文串，只考虑字母和数字字符，可以忽略字母的大小写。
+//
+//说明：本题中，我们将空字符串定义为有效的回文串。
+//
+//示例1：
+//输入 : "A man, a plan, a canal: Panama"
+//输出 : true
+//示例2：
+//输入 : "race a car"
+//输出 : false
+
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+	bool isLetter(char ch)
+	{
+		if ((ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= '9'))
+		{
+			return true;
+		}
+		return false;
+	}
+
+	bool isPalindrome(string s) {
+		// 先小写字母转换成大写，再进行判断
+		for (auto& ch : s)
+		{
+			if (ch >= 'A' && ch <= 'Z')
+				ch += 32;
+		}
+
+		int left = 0;
+		int right = s.size() - 1;
+
+		while (left < right)
+		{
+			while (left < right && !isLetter(s[left]))
+				left++;
+
+			while (left < right && !isLetter(s[right]))
+				right--;
+
+			if (s[left] != s[right])
+				return false;
+
+			left++;
+			right--;
+		}
+
+		return true;
+	}
+};
