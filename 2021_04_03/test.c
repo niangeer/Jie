3ae87b3a1d35c6e6aebdab95074a723183692550
niangeer <898609031@qq.com>
2021-04-03 23:21:23 +0800

    gcc/g++

---
+++ 2021_04_03/test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#define MAX 7
+
+int main()
+{
+  int a = MAX;
+  int b = a;
+  int c = b;
+ // int d = MAX;
+ // int e = MAX;
+  printf("%d\n",a);
+  printf("%d\n",b);
+  printf("%d\n",c);
+  return 0;
+}
